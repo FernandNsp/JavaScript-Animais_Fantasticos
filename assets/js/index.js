bbfc12f1ce6cb175e33559b7ade696c8d2bd670f
Fernanda Sene <fehsene@gmail.com>
2022-01-09 16:16:09 -0300

    Isolando função initAccordion

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,13 +21,22 @@ function initTabAnimals(){
 }
 initTabAnimals()
 
-const accordionFaq = document.querySelectorAll('.js-accordion dt')
+function initAccordion(){
+	const accordionFaq = document.querySelectorAll('.js-accordion dt')
+	const activeClass = 'ativo'
 
-function activeAccordion(){
-	this.classList.toggle('ativo')
-	this.nextElementSibling.classList.toggle('ativo')
+	if(accordionFaq.length){
+		accordionFaq[0].classList.add(activeClass)
+		accordionFaq[0].nextElementSibling.classList.add(activeClass)
+
+		function activeAccordion(){
+			this.classList.toggle(activeClass)
+			this.nextElementSibling.classList.toggle(activeClass)
+		}
+
+		accordionFaq.forEach((item) => {
+			item.addEventListener('click', activeAccordion)
+		})
+	}
 }
-
-accordionFaq.forEach((item) => {
-	item.addEventListener('click', activeAccordion)
-})
+initAccordion()
